3e6ab22406dbc627cc6de131f88c882ebd64a884
farlliant <160444686+farlliant@users.noreply.github.com>
2026-09-24 20:59:23 +0700

    feat(modul-2): add conditional assignment example

---
+++ Modul 2/constAss.java
@@ -0,0 +1,8 @@
+public class constAss {
+    public static void main(String[] args) {
+        String s = "filkom";
+        String val = (s == "filkom") ? "Brawijaya" : "null";
+
+        System.out.println(s + " " + val);
+    }
+}
